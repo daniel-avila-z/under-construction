7316cbe46b74fc6bc7488dbbf8571ee4d3d2b577
Daniel Avila <roicesdaniel@gmail.com>
2023-12-02 20:48:01 -0400

    Update dependencies, remove next.svg and vercelsvg, add favicon.ico, modify some styles

--- src/app/background.jsx
+++ src/app/background.jsx
@@ -1,12 +1,14 @@
 // import Image from 'next/image'
-import Stars from './Stars'
+// import Stars from './Stars'
+import Link from 'next/link'
 import styles from './styles/styles.module.css'
 // import mountains from '../../public/backg.jpg'
 
 const BackgroundPage = () => (
-  <div>
-    <Stars className={styles.bgWrap} />
-    {/* <div className={styles.bgWrap}>
+  <>
+    <div>
+      {/* <Stars className={styles.bgWrap} /> */}
+      {/* <div className={styles.bgWrap}>
       <Image
         alt='Mountains's
         src={mountains}
@@ -19,15 +21,27 @@ const BackgroundPage = () => (
         }}
       />
     </div> */}
-    <div className={styles.textMainContainer}>
-      <h1 className={styles.h1}><span className={styles.span}>New Website</span> <br />      <span className={styles.span}>Coming Soon </span> <br /> <span class={styles.loader} /></h1>
-    </div>
-    {/* <p className={styles.bgText}>
+      <div className={styles.textMainContainer}>
+        <h1 className={styles.h1}><span className={styles.span}>New Website</span>
+          <br />
+          <span className={styles.loader}>...</span>
+          <br />
+          <span className={styles.span}>Coming Soon </span>
+        </h1>
+      </div>
+      {/* <p className={styles.bgText}>
       Image Component
       <br />
       as a Background
     </p> */}
-  </div>
+    </div>
+    <div className={styles.contactContainer}>
+      <h2 className={styles.contactH2}>Contact:</h2>
+      <p>+1 (786) 471-7050</p>
+      <p className={styles.contactEmail}>Email: jfprojects871@gmail.com</p>
+      <Link blank href='https://www.instagram.com/_jfprojects/?hl=es'>Instagram: _jfprojects</Link>
+    </div>
+  </>
 )
 
 export default BackgroundPage
